c27a46411735bf518cb3f4d6ed1f26b5ec75a7e2
m.honda0119 <rohtom878dara@gmail.com>
2026-04-04 23:54:57 +0900

    Quartz sync: Apr 4, 2026, 11:54 PM

--- content/index.md
+++ content/index.md
@@ -6,10 +6,5 @@ tags:
 
 # Home
 
-Welcome to my digital garden.
 
-This site is powered by [Quartz](https://quartz.jzhao.xyz/) and [Obsidian](https://obsidian.md/).
 
-## Featured Posts
-
-Articles marked with `#Publish` are published here.
--- quartz/components/TagList.tsx
+++ quartz/components/TagList.tsx
@@ -3,7 +3,7 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import { classNames } from "../util/lang"
 
 const TagList: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
-  const tags = fileData.frontmatter?.tags
+  const tags = fileData.frontmatter?.tags?.filter(tag => tag.toLowerCase() !== "publish")
   if (tags && tags.length > 0) {
     return (
       <ul class={classNames(displayClass, "tags")}>
